83c7ac412d937fe93ce675d74c73a13b9ca5163e
Julian Ortiz <julian.ortiz@pragma.com.co>
2025-02-26 11:03:30 -0500

    spring initalizr

---
+++ src/main/java/com/on_class/capability/CapabilityApplication.java
@@ -0,0 +1,13 @@
+package com.on_class.capability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapabilityApplication.class, args);
+	}
+
+}
